f4e51fecf3636121fe0b4dcc7ec3fe81901df58c
yash03112000 <yashag@iitk.ac.in>
2021-03-10 01:55:18 +0530

    ready to deploy

--- pages/Plog/[id}.js
+++ pages/Plog/[id}.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import styles from '../../styles/Home.module.css'
 import React, {useState} from 'react';
 import {TextField,Button,Typography} from '@material-ui/core';
 import { useRouter } from 'next/router'
