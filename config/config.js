7c8bd878475fb0a3f43d85aa4c65a077af1d3421
yash03112000 <yashag@iitk.ac.in>
2021-03-10 02:04:56 +0530

    second

--- config/config.js
+++ config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     db_dev:'mongodb://localhost:27017/minlinks',
-    db: 'mongodb+srv://yash1234:<yash1234>@cluster0.9bnae.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    // db: ''
   }
 
   
--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:8080' : 'https://your_deployment.server.com';
+export const server = dev ? 'http://localhost:8080' : 'https://minlinks.herokuapp.com';
